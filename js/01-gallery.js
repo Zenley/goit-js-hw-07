71d1f35a4509194f7d5a5f5ee5b70c4be58aeb7b
Zenley <valy468@gmail.com>
2024-06-23 21:44:43 +0300

    .
    .

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
+const galleryContainer = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems.map(({ preview, original, description }) => {
+  return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>
+  `;
+}).join('');
+
+galleryContainer.innerHTML = galleryMarkup;
+
+galleryContainer.addEventListener('click', event => {
+  event.preventDefault();
+
+  const target = event.target;
+  if (target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const largeImageURL = target.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <img src="${largeImageURL}" width="800" height="600">
+  `);
+
+  instance.show();
+
+  // Închiderea ferestrei modale cu tasta Escape
+  const closeModalOnEscape = event => {
+    if (event.code === 'Escape') {
+      instance.close();
+      document.removeEventListener('keydown', closeModalOnEscape);
+    }
+  };
+
+  document.addEventListener('keydown', closeModalOnEscape);
+});
 
 console.log(galleryItems);
+
+
+
